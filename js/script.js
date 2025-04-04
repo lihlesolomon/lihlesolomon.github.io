c99372b5320d0d5b49e1c9a4cecbad929ca8c548
Lihle Solomon <62881293+lihlesolomon@users.noreply.github.com>
2025-04-04 14:18:49 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,3 @@
-// Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyBriONGl6ZtXSdtJ-fFFqxtpGVp_OPcqJg",
-    authDomain: "nickysbirthday-1e188.firebaseapp.com",
-    databaseURL: "https://nickysbirthday-1e188-default-rtdb.firebaseio.com",
-    projectId: "nickysbirthday-1e188",
-    storageBucket: "nickysbirthday-1e188.appspot.com",
-    messagingSenderId: "731449038490",
-    appId: "1:731449038490:web:67139e3b425b60253fb123"
-};
-
-// Initialize Firebase
-const app = firebase.initializeApp(firebaseConfig);
-const database = firebase.database();
-- 4. js/script.js
-```javascript
 // Gift Data
 const gifts = [
     { 
